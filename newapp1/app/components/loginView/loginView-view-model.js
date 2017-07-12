798851bdb0af876f7892eb2ea63e7746faee22a3
robert heslar <heslarr@liro.com>
2017-07-12 22:04:07 +0000

    Changes made by robert heslar (heslarr@liro.com).

---
+++ newapp1/app/components/loginView/loginView-view-model.js
@@ -0,0 +1,34 @@
+'use strict';
+var ViewModel,
+    Observable = require('data/observable').Observable;
+// additional requires
+
+ViewModel = new Observable({
+
+    backButtonHidden: true,
+
+    pageTitle: 'Login',
+
+    events: {
+        loginViewModelSubmit: 'loginViewModelSubmit',
+    },
+
+    onloginViewModelFormSubmit: function() {
+        this.notify({
+            eventName: this.events.loginViewModelSubmit
+        });
+    },
+
+    txtUserName: '',
+
+    txtPassword: '',
+
+    // additional properties
+
+});
+
+// START_CUSTOM_CODE_loginView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_loginView
+module.exports = ViewModel;
---
+++ newapp1/app/components/loginView/loginView.js
@@ -0,0 +1,66 @@
+'use strict';
+var isInit = true,
+    helpers = require('../../utils/widgets/helper'),
+    navigationProperty = require('../../utils/widgets/navigation-property'),
+
+    gestures = require('ui/gestures'),
+    dataService = require('../../dataProviders/backendServices'),
+
+    // additional requires
+
+    viewModel = require('./loginView-view-model');
+
+function onRequestSuccess() {
+    alert("user Logged in");
+    helpers.navigate("components/homeView/homeView");
+}
+
+function onRequestFail(err) {
+    alert(JSON.stringify(err));
+    return err;
+}
+
+function saveData() {
+    var data = dataService.data('User');
+
+    data.save({
+
+            UserName: viewModel.get('txtUserName'),
+
+            PassWord: viewModel.get('txtPassword'),
+
+            // save properties
+
+        })
+        .then(onRequestSuccess.bind(this))
+        .catch(onRequestFail.bind(this));
+}
+
+function onloginViewModelFormSubmit() {
+    saveData();
+}
+
+// additional functions
+
+function pageLoaded(args) {
+    var page = args.object;
+
+    helpers.platformInit(page);
+    page.bindingContext = viewModel;
+    // additional pageLoaded
+
+    if (isInit) {
+        isInit = false;
+
+        viewModel.on(viewModel.events.loginViewModelSubmit, onloginViewModelFormSubmit);
+
+        // additional pageInit
+
+    }
+}
+
+// START_CUSTOM_CODE_loginView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_loginView
+exports.pageLoaded = pageLoaded;
